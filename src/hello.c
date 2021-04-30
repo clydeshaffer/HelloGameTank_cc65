317243b7e74c3481cb66b5d033b2715861878c93
Clyde Shaffer <clydeshaffer@gmail.com>
2021-04-30 01:29:36 -0400

    comment explaining CLS()

--- src/hello.c
+++ src/hello.c
@@ -2,6 +2,13 @@
 
 extern void wait ();
 
+/*
+ * Because the high bit of WIDTH/HEIGHT has special meaning,
+ * the maximum size of a blitter operation is 127x127
+ * which means a full screen clear requires four calls.
+ * However, because the bottom five or so rows don't show up on a TV
+ * we can skip those and only perform two draw calls.
+ */
 void CLS(char c) {
     vram[VX] = 0;
     vram[VY] = 0;
@@ -16,12 +23,6 @@ void CLS(char c) {
     vram[VY] = 0;
     vram[WIDTH] = 1;
     vram[HEIGHT] = SCREEN_HEIGHT-1;
-    vram[START] = 1;
-    wait ();
-    vram[VX] = 0;
-    vram[VY] = SCREEN_HEIGHT-1;
-    vram[WIDTH] = SCREEN_WIDTH-1;
-    vram[HEIGHT] = 1;
     vram[START] = 1;
     wait ();
 }
@@ -39,23 +40,24 @@ void FillRect(char x, char y, char w, char h, char c) {
 }
 
 int main () {
-    char col = 0, row = 0;
+    char col = 30, row = 20;
     int dx = 1, dy = 1;
     *dma_flags = DMA_NMI | DMA_ENABLE | DMA_IRQ | DMA_TRANS;
 
   while (1) {                                     //  Run forever
     CLS(3);
+    FillRect(8, 8, 112, 92, 2);
     FillRect(col, row, 8, 8, 92);
     col += dx;
     row += dy;
-    if(col == 1) {
+    if(col == 8) {
         dx = 1;
-    } else if(col == 111) {
+    } else if(col == 112) {
         dx = -1;
     }
-    if(row == 1) {
+    if(row == 8) {
         dy = 1;
-    } else if(row == 100) {
+    } else if(row == 92) {
         dy = -1;
     }
     wait();
